09312a693687ad0b5e8976f26b65e8b032c8d273
xchall <macsnebyvalov@yandex.ru>
2024-10-23 16:38:28 +0300

    ddddd

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 int main(){
   // код с комментарием :)
-  std:: string user_name;
-  std:: cout << "Enter user name: ";
-  std:: cin >> user_name;
-  std:: cout << "Hello World from " << user_name;
+  std::string user_name;
+  std::cout << "Enter user name: ";
+  std::cin >> user_name;
+  std::cout << "Hello World from " << user_name;
 }
