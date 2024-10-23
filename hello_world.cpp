c746088e3006774f0fe68f556ba42fafc44718a8
xchall <macsnebyvalov@yandex.ru>
2024-10-23 12:59:56 +0300

    Приветствие мира от лица юзера

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,7 +1,9 @@
-#include <iostream> 
+#include <iostream>
 using namespace std;
 int main()
 {
-    cout << "Hello World";
+    string user_name;
+    cout << "Enter user name: ";
+    cin >> user_name;
+    cout << "Hello World from " << user_name;
 }
-
