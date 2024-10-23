2dd7cc5fab6894382e762eddf4da8e1c916de387
xchall <macsnebyvalov@yandex.ru>
2024-10-23 19:06:33 +0300

    Устраняем конфликты

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 int main(){
-  // code with comment 
-  std:: string user_name; //some other comment
-  std:: cout << "Enter user name: ";
-  std:: cin >> user_name;
-  std:: cout << "Hello World from " << user_name;
+  // код с комментарием :)
+  std::string user_name;
+  std::cout << "Enter user name: ";
+  std::cin >> user_name;
+  std::cout << "Hello World from " << user_name;
 }
